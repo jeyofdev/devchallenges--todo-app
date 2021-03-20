4f7471c0fb9f9b68fdc01cc4c164913795c7451f
jeyofdev <jgregoire.pro@gmail.com>
2021-03-20 18:19:55 +0100

    style : initialize the default css

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="container">App</div>
+        </div>
+    );
+};
+
+export default App;
